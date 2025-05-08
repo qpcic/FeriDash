ad11a8f82c484d804915c49356bdac4530952cfe
MartinFerk <martinferk434@gmail.com>
2025-05-08 17:06:31 +0200

    Dodan level prgress slider

---
+++ Assets/Scripts/LevelProgress.cs
@@ -0,0 +1,41 @@
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class LevelProgress : MonoBehaviour
+{
+    public Transform player;
+    public Transform levelEnd;
+    public Slider progressBar;
+    public TextMeshProUGUI progressText;
+    public bool levelComplete = false;
+
+    private float totalDistance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        totalDistance = Vector3.Distance(player.position, levelEnd.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelComplete)
+        {
+            progressBar.value = 1f;
+            progressText.text = "100%";
+            return;
+        }
+        float distancePassed = Vector3.Distance(player.position, levelEnd.position);
+        float progress = 1f - (distancePassed / totalDistance);
+        progressBar.value = Mathf.Clamp01(progress);
+        int percent = Mathf.RoundToInt(progress * 100f);
+        progressText.text = percent + "%";
+    }
+
+    public void CompleteLevel()
+    {
+        levelComplete = true;
+        
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,9 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    public void PlayGame()
+    public void LevelSelect()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("LevelSelect");
 
     }
 
