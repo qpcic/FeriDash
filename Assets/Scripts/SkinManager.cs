818d3eae322502b06faa43e12e677524a909df94
Jan Kupčič <kupcic.jan@gmail.com>
2025-05-11 21:19:20 +0200

    Highlight izbranega skina

--- Assets/Scripts/SkinManager.cs
+++ Assets/Scripts/SkinManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class SkinManager : MonoBehaviour
@@ -5,6 +6,10 @@ public class SkinManager : MonoBehaviour
     public Sprite[] availableSkins; // Assign in Inspector
     public SpriteRenderer targetRenderer; // The character preview or real player
 
+    public TMP_Text text1;
+    public TMP_Text text2;
+    public TMP_Text text3;
+    
     private const string SkinKey = "SelectedSkin";
     private int currentSkinIndex = 0;
 
@@ -17,7 +22,22 @@ public class SkinManager : MonoBehaviour
 
     public void SelectSkin(int index)
     {
+        text1.color = Color.black;
+        text2.color = Color.black;
+        text3.color = Color.black;
         currentSkinIndex = index;
+        if (currentSkinIndex == 0)
+        {
+            text1.color = Color.white; 
+        }
+        if (currentSkinIndex == 1)
+        {
+            text2.color = Color.white;
+        }
+        if (currentSkinIndex == 2)
+        {
+            text3.color = Color.white;
+        }
         targetRenderer.sprite = availableSkins[index];
         Debug.Log("Selected skin index (preview only): " + index);
     }
