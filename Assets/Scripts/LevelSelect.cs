edcc5a9e8d6aa35bfff0699424a236ddd82a856a
Paskal Trstenjak <paskal.trstenjak@hotmail.com>
2025-06-09 01:31:26 +0200

    Resolved conflicts with LevelSelect.cs

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -1,23 +1,48 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
+    public AudioSource buttonClickSound;
+
     public void OpenMenu()
     {
-        Debug.Log("Click Test");
-        SceneManager.LoadScene("MainMenu");
+        PlayClickSound();
+        Invoke(nameof(LoadMainMenu), 0.4f); // zakasnjen prehod
     }
-    
+
     public void PlayLevelOne()
     {
-        SceneManager.LoadScene("SampleScene");
-
+        PlayClickSound();
+        Invoke(nameof(LoadLevelOne), 0.4f);
     }
 
     public void PlayLevelTwo()
     {
-        SceneManager.LoadScene("SampleScene2");
+        PlayClickSound();
+        Invoke(nameof(LoadLevelTwo), 0.4f);
+    }
 
+    private void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void LoadLevelOne()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    private void LoadLevelTwo()
+    {
+        SceneManager.LoadScene("SampleScene2");
+    }
+
+    private void PlayClickSound()
+    {
+        if (buttonClickSound != null)
+            buttonClickSound.Play();
+        else
+            Debug.LogWarning("ButtonClickSound ni nastavljen!");
     }
 }
